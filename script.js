94cda97f2c9cc8e0d74986675ef50cc456963fa1
mhayv <mhayvine.capulong@mds.ac.nz>
2026-06-15 00:16:12 +1200

    editing the landing

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-const colors = ["#00B088", "#FAA819", "#1ABFF1", "#FFCB05", "#959BCA", "#F495AE"];
+const colors = ["#FF4FA3",
+  "#FF8A00",
+  "#00C8FF",
+  "#C63EFF"];
 
 let lastX = 0;
 let lastY = 0;
@@ -33,4 +36,95 @@ document.addEventListener("mousemove", (e) => {
   setTimeout(() => {
     trail.remove();
   }, 2000);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("sprinkle system running");
+
+const container = document.getElementById("sprinkleContainer");
+
+const sprinkleColors = [
+  "#FF4FA3",
+  "#FF8A00",
+  "#00C8FF",
+  "#C63EFF",
+  
+];
+
+function playWave() {
+  gsap.fromTo(".letter",
+    { y: 0 },
+    {
+      y: -20,
+      duration: 0.25,
+      stagger: 0.03,
+      repeat: 1,
+      yoyo: true,
+      ease: "power2.out"
+    }
+  );
+}
+
+function createSprinkle(x, y) {
+
+  const s = document.createElement("div");
+  s.classList.add("sprinkle");
+
+  s.style.background =
+    sprinkleColors[Math.floor(Math.random() * sprinkleColors.length)];
+
+  s.style.left = x + "px";
+  s.style.top = y + "px";
+
+  container.appendChild(s);
+
+  gsap.to(s, {
+    y: window.innerHeight + 120,
+    x: gsap.utils.random(-120, 120),
+    rotation: gsap.utils.random(-360, 360),
+    duration: gsap.utils.random(2, 4),
+    ease: "none",
+    onComplete: () => s.remove()
+  });
+}
+
+function sprinkleRain(amount = 150) {
+
+  for (let i = 0; i < amount; i++) {
+
+    setTimeout(() => {
+
+      createSprinkle(
+        Math.random() * window.innerWidth,
+        -20
+      );
+
+    }, i * 25);
+
+  }
+}
+playWave();
+sprinkleRain();
+
+const joy = document.querySelector(".joyImg");
+
+joy.addEventListener("mouseenter", () => {
+
+  playWave();
+  sprinkleRain(80);
+
 });
